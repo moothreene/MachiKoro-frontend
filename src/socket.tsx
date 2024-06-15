a19e5a92f8d22dc7a5ce27b7f42d4ffdf2cc3ac4
Martin <moothreene@gmail.com>
2024-06-15 16:42:52 +0300

    remove dotenv

--- src/socket.tsx
+++ src/socket.tsx
@@ -1,5 +1,3 @@
 import { io } from 'socket.io-client';
-import dotenv from 'dotenv';
-dotenv.config();
-const URL = process.env.REACT_APP_URL as string;
+const URL = process.env.REACT_APP_URL || 'http://localhost:4000' as string;
 export const socket = io(URL,{autoConnect: false});
