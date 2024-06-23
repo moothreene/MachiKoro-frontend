2456696c932ade77027f3930bbdc6798ad2bb06c
Martin <moothreene@gmail.com>
2024-06-23 14:23:19 +0300

    Update Game.tsx

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -152,8 +152,8 @@ function Game({
           className="background-gradient enemy"
           sx={{
             position: 'absolute',
-            width: '100%',
-            height: '100%',
+            width: '100vw',
+            height: '100vh',
             top: 0,
             left: 0,
             background:
@@ -167,8 +167,8 @@ function Game({
           className="background-gradient player"
           sx={{
             position: 'absolute',
-            width: '100%',
-            height: '100%',
+            width: '100vw',
+            height: '100vh',
             bottom: 0,
             left: 0,
             background:
