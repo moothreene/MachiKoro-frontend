99c7bd912d4ac079104b424c75146a4c4f0a7731
Martin <moothreene@gmail.com>
2024-06-23 16:00:32 +0300

    Update Game.tsx

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -122,7 +122,11 @@ function Game({
     if (2 - (gameState.currentMove % 2) !== player)
       return alert('Not your turn!');
     if (stage < 2) return alert('You have to roll the dice first!');
-    if (lastRoll.length > 1 && lastRoll[0] === lastRoll[1] && gameState.players[player].properties['amusement_park'] > 0) {
+    if (
+      lastRoll.length > 1 &&
+      lastRoll[0] === lastRoll[1] &&
+      gameState.players[player].properties['amusement_park'] > 0
+    ) {
       return socket.emit('nextTurn', 2);
     }
     socket.emit('nextTurn', 1);
