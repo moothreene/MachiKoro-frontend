620cbb35d3a802667f85095cbe1f745e7cf27568
Martin <moothreene@gmail.com>
2024-07-03 15:09:20 +0300

    added simple AI (WIP)

--- src/AI/AILogic.tsx
+++ src/AI/AILogic.tsx
@@ -1,4 +1,6 @@
+import { RefObject, useEffect, useRef } from 'react';
 import { Cards, GameData, Player, Roll } from '../components/Types/GameTypes';
+import { Properties } from '../data/Properties';
 import {
   ConfirmRollHelper,
   calculateRoll,
@@ -8,45 +10,60 @@ import {
 } from '../utils/helper';
 
 export const AITurn = async (
-  gameState: GameData,
+  gameState: RefObject<GameData>,
   gameStateSetter: (value: React.SetStateAction<GameData>) => void,
-  AIPlayer: Player
+  AIPlayer: Player,
+  setHighlighted: (value: string | null) => void
 ) => {
-  function stageZero() {
-    const bestRoll = getAIBestRoll(gameState, AIPlayer);
+  const strategies = [
+    'random',
+    'one_die',
+    'bakery',
+    'convenience',
+    'cheese',
+    'furniture',
+  ];
+
+  let strategy = strategies[Math.floor(Math.random() * strategies.length)];
+
+  function stageZero(current: GameData | null) {
+    if (!current) return;
+    const bestRoll = getAIBestRoll(current, AIPlayer);
     const diceAmount = bestRoll[0] > bestRoll[1] ? 1 : 2;
-    spRoll(diceAmount, AIPlayer, gameState, gameStateSetter);
+    spRoll(diceAmount, AIPlayer, current, gameStateSetter);
   }
 
   function stageOne() {
     ConfirmRollHelper(gameStateSetter);
   }
 
-  function stageTwo() {
-    const prop = getAIBestProp(gameState, AIPlayer);
+  function stageTwo(current: GameData | null) {
+    if (!current) return;
+    strategy = evalStrategy(current, AIPlayer, strategy);
+    const prop = getAIBestProp(current, AIPlayer, 'one_die');
     if (prop) {
       onBuyHelper({ player: AIPlayer, property: prop }, gameStateSetter);
+      setHighlighted(prop);
     }
   }
 
-  function stageThree() {
+  function stageThree(current: GameData | null) {
+    if (!current) return;
     if (
-      gameState.lastRoll.length > 1 &&
-      gameState.lastRoll[0] === gameState.lastRoll[1] &&
-      gameState.players[AIPlayer].properties['amusement_park'] > 0
+      current.lastRoll.length > 1 &&
+      current.lastRoll[0] === current.lastRoll[1] &&
+      current.players[AIPlayer].properties['amusement_park'] > 0
     ) {
       return onNextTurnHelper(2, gameStateSetter);
     }
     return onNextTurnHelper(1, gameStateSetter);
   }
 
-  if (gameState.stage !== 0) return;
-  setTimeout(stageZero, 1000);
-  setTimeout(stageOne, 2000);
-  //TODO add delay for AI to roll
-  //TODO add reroll logic
-  setTimeout(stageTwo, 3000);
-  setTimeout(stageThree, 4000);
+  if (gameState.current?.stage !== 0) return;
+  setTimeout(() => stageZero(gameState.current), 1000);
+  setTimeout(() => stageOne(), 2000);
+  setTimeout(() => stageTwo(gameState.current), 3000);
+  setTimeout(() => stageThree(gameState.current), 4000);
 };
 
 function getAIBestRoll(gameState: GameData, targetPlayer: Player) {
@@ -94,7 +111,246 @@ function getAllDiceResults(num: 1 | 2) {
 
 function getAIBestProp(
   gameState: GameData,
+  targetPlayer: Player,
+  strategy: string
+): keyof Cards | null {
+  if (strategy === 'random') {
+    return strategyRandom(gameState, targetPlayer);
+  }
+  if (strategy === 'one_die') {
+    return strategyOneDie(gameState, targetPlayer);
+  }
+  if (strategy === 'bakery') {
+    //
+  }
+  return null;
+}
+
+function evalStrategy(
+  gameState: GameData,
+  targetPlayer: Player,
+  strategy: string
+): string {
+  const enemyPlayer = targetPlayer === 1 ? 2 : 1;
+  let options = [] as string[];
+  switch (strategy) {
+    case 'random':
+      return 'random';
+    case 'bakery':
+      if (
+        gameState.players[enemyPlayer].properties['bakery'] <= 3 &&
+        gameState.players[targetPlayer].properties['bakery'] >=
+          gameState.players[enemyPlayer].properties['bakery']
+      ) {
+        return 'bakery';
+      } else {
+        return changeStrategy(gameState, targetPlayer);
+      }
+    case 'convenience':
+      if (
+        gameState.players[enemyPlayer].properties['convenience_store'] <= 2 &&
+        gameState.players[targetPlayer].properties['convenience_store'] >=
+          gameState.players[enemyPlayer].properties['convenience_store']
+      ) {
+        return 'convenience';
+      } else {
+        return changeStrategy(gameState, targetPlayer);
+      }
+    case 'cheese':
+      if (
+        gameState.players[enemyPlayer].properties['ranch'] <= 2 &&
+        gameState.players[targetPlayer].properties['ranch'] >=
+          gameState.players[enemyPlayer].properties['ranch']
+      ) {
+        return 'cheese';
+      } else {
+        return changeStrategy(gameState, targetPlayer);
+      }
+    case 'furniture':
+      if (
+        gameState.players[enemyPlayer].properties['forest'] <= 2 &&
+        gameState.players[targetPlayer].properties['forest'] >=
+          gameState.players[enemyPlayer].properties['forest']
+      ) {
+        return 'furniture';
+      } else {
+        return changeStrategy(gameState, targetPlayer);
+      }
+    case 'one_die':
+      return 'one_die';
+  }
+  return '';
+}
+
+function changeStrategy(gameState: GameData, targetPlayer: Player): string {
+  let options = ['one_die'] as string[];
+  if (
+    gameState.players[targetPlayer].properties['bakery'] +
+      gameState.store['bakery'] >=
+    6
+  ) {
+    options.push('bakery');
+  }
+  if (
+    gameState.players[targetPlayer].properties['convenience_store'] +
+      gameState.store['convenience_store'] >=
+    6
+  ) {
+    options.push('convenience');
+  }
+  if (
+    gameState.players[targetPlayer].properties['ranch'] +
+      gameState.store['ranch'] >=
+    6
+  ) {
+    options.push('cheese');
+  }
+  if (
+    gameState.players[targetPlayer].properties['forest'] +
+      gameState.store['forest'] >=
+    6
+  ) {
+    options.push('furniture');
+  }
+  return options[Math.floor(Math.random() * options.length)];
+}
+
+function strategyRandom(
+  gameState: GameData,
   targetPlayer: Player
 ): keyof Cards | null {
-  return null;
+  const options = [null] as (keyof Cards | null)[];
+  for (let prop of Object.keys(gameState.store) as (keyof Cards)[]) {
+    if (
+      gameState.store[prop] > 0 &&
+      gameState.players[targetPlayer].money >= Properties[prop].cost
+    ) {
+      options.push(prop);
+    }
+  }
+  return options[Math.floor(Math.random() * options.length)];
+}
+
+function strategyOneDie(
+  gameState: GameData,
+  targetPlayer: Player
+): keyof Cards | null {
+  let options = [] as (keyof Cards)[];
+  const target = {
+    wheat_field: 2,
+    ranch: 2,
+    forest: 2,
+    cafe: 2,
+    convenience_store: 2,
+    bakery: 2,
+    family_restaraunt: 1,
+    tv_station: 1,
+    train_station: 0.5,
+    shopping_mall: 5,
+    amusement_park: 10,
+    radio_tower: 10,
+  };
+  const singleProps = [
+    'publisher',
+    'tv_station',
+    'tax_office',
+    'train_station',
+    'shopping_mall',
+    'amusement_park',
+    'radio_tower',
+  ];
+  const enemyPlayer = targetPlayer === 1 ? 2 : 1;
+  for (let prop of Object.keys(target) as (keyof typeof target)[]) {
+    if (gameState.store[prop] === 0) {
+      continue;
+    }
+    if (gameState.players[targetPlayer].money < Properties[prop].cost) {
+      continue;
+    }
+    if (target[prop] <= gameState.players[targetPlayer].properties[prop]) {
+      continue;
+    }
+    if (
+      singleProps.includes(prop) &&
+      gameState.players[targetPlayer].properties[prop] > 0
+    ) {
+      continue;
+    }
+    if (
+      prop === 'family_restaraunt' &&
+      gameState.players[enemyPlayer].properties['train_station'] === 0
+    ) {
+      continue;
+    }
+    let propsArr = new Array(
+      (target[prop] - gameState.players[targetPlayer].properties[prop]) * 2
+    ).fill(prop);
+    options.push(...propsArr);
+  }
+  if (options.length === 0) {
+    return null;
+  }
+  return options[Math.floor(Math.random() * options.length)];
+}
+
+function strategyBakery(
+  gameState: GameData,
+  targetPlayer: Player
+): keyof Cards | null {
+  let options = [] as (keyof Cards)[];
+  const target = {
+    wheat_field: 2,
+    ranch: 2,
+    forest: 2,
+    cafe: 2,
+    convenience_store: 2,
+    bakery: 2,
+    family_restaraunt: 1,
+    tv_station: 1,
+    train_station: 1,
+    shopping_mall: 1,
+    amusement_park: 1,
+    radio_tower: 1,
+  };
+  const singleProps = [
+    'publisher',
+    'tv_station',
+    'tax_office',
+    'train_station',
+    'shopping_mall',
+    'amusement_park',
+    'radio_tower',
+  ];
+  const enemyPlayer = targetPlayer === 1 ? 2 : 1;
+  for (let prop of Object.keys(target) as (keyof typeof target)[]) {
+    if (gameState.store[prop] === 0) {
+      continue;
+    }
+    if (gameState.players[targetPlayer].money < Properties[prop].cost) {
+      continue;
+    }
+    if (target[prop] <= gameState.players[targetPlayer].properties[prop]) {
+      continue;
+    }
+    if (
+      singleProps.includes(prop) &&
+      gameState.players[targetPlayer].properties[prop] > 0
+    ) {
+      continue;
+    }
+    if (
+      prop === 'family_restaraunt' &&
+      gameState.players[enemyPlayer].properties['train_station'] === 0
+    ) {
+      continue;
+    }
+    let propsArr = new Array(
+      (target[prop] - gameState.players[targetPlayer].properties[prop]) * 2
+    ).fill(prop);
+    options.push(...propsArr);
+  }
+  if (options.length === 0) {
+    return null;
+  }
+  return options[Math.floor(Math.random() * options.length)];
 }
