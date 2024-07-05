e4b3fe1aa9dc60fc048e969195714bc4d637cd35
Martin <moothreene@gmail.com>
2024-07-05 13:47:04 +0300

    formatting

--- src/components/PlayerState.tsx
+++ src/components/PlayerState.tsx
@@ -82,7 +82,9 @@ function PlayerState({
               <Money
                 size={fontSize}
                 amount={money}
-                zIndex={tutorialStage === 3 || spTutorialStage === 3 ? 100 : 'auto'}
+                zIndex={
+                  tutorialStage === 3 || spTutorialStage === 3 ? 100 : 'auto'
+                }
               />
               {money_to_earn !== 0 && (
                 <>
@@ -128,7 +130,7 @@ function PlayerState({
                 amount={value}
                 name={key}
                 isDisabled={false}
-                opaque = {false}
+                opaque={false}
                 image={images[key]}
                 onClick={() => {}}
               />
